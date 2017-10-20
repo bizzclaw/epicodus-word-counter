2960ccea333d94354108b3137d37d00548b9b965
Joseph Tomlinson <bizzclaw1@gmail.com>
2017-10-20 09:10:43 -0700

    layout base project

---
+++ WordCounter.Tests/WordCounter.Models.Tests/Words.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Models.Tests
+{
+  [TestClass]
+  public class WordsTest
+  {
+    [TestMethod]
+    public void Method_Description_ExpectedValue()
+    {
+
+    }
+  }
+}
